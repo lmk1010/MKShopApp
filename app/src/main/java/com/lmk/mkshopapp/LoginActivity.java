c44c54b79fc9949375245fcabcf70bd83c09c0ee
lmk <lmkbnb0@gmail.com>
2019-01-17 20:27:06 +0800

    [UPDATE] 新增登陆界面

---
+++ app/src/main/java/com/lmk/mkshopapp/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.lmk.mkshopapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    //安卓的生命周期 启动
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
